07eb27307419b9007b2514a4531b91da95646d8a
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-03-08 21:01:46 +0200

    add mvc proj

---
+++ Fruitables/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fruitables.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult ShopDetails()
+        {
+            return View();
+        }
+    }
+}
