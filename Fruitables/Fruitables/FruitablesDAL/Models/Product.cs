a38df469e1c592bba454bbcea7c0076a0e7d12fd
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-03-10 21:09:52 +0200

    update img structure

--- Fruitables/Fruitables/FruitablesDAL/Models/Product.cs
+++ Fruitables/Fruitables/FruitablesDAL/Models/Product.cs
@@ -25,6 +25,8 @@ public partial class Product
 
     public int SellerId { get; set; }
 
+    public string Image { get; set; }
+
     public virtual Category Category { get; set; }
 
     public virtual ICollection<FeedBack> FeedBacks { get; set; } = new List<FeedBack>();
