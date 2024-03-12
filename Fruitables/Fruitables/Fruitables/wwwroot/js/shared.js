aad96418893736415a682be683a036bbbb083ea3
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-03-12 02:40:47 +0200

    push - stable

---
+++ Fruitables/Fruitables/Fruitables/wwwroot/js/shared.js
@@ -0,0 +1,29 @@
+﻿$(document).ready( function () {
+
+    const data = localStorage.getItem('cart');
+
+    $("#cart-btn").click(function () {
+
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/Cart/Cart',
+            dataType: "json",
+            traditional:true,
+            contentType: 'application/json',
+            data: { data: data },
+            success: function (response) {
+                prompt("Success");
+            },
+            error: function (error) {
+                prompt("Error");
+            }
+        });
+    });
+
+});
+
+
+
+
