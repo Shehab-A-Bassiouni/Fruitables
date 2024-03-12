f786e50da97b86ee27d4b7a4d886f66a97ec49f5
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-03-12 02:35:05 +0200

    psh

---
+++ Fruitables/FruitablesBL/EntityManagement/Interface/InterISellerRepo.cs
@@ -0,0 +1,15 @@
+﻿using FruitablesBL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitablesBL.EntityManagement.Interface
+{
+    public interface ISellerRepo
+    {
+        List<SellerVM> getALlshops();
+        List<SellerVM> getALlshops(int pageNumber);
+    }
+}
---
+++ Fruitables/FruitablesBL/EntityManagement/Repository/CartRepo.cs
@@ -0,0 +1,47 @@
+﻿using FruitablesBL.EntityManagement.Interface;
+using FruitablesBL.Mapping;
+using FruitablesBL.ViewModels;
+using FruitablesDAL.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitablesBL.EntityManagement.Repository
+{
+    public class CartRepo //: ICartRepo
+    {
+     
+
+        private readonly FRUITABLESContext DbContext;
+        public CartRepo(FRUITABLESContext _db)
+        {
+            DbContext = _db;
+
+        }
+
+        public CartRepo()
+        {
+
+        }
+
+        public CartVM ToCartVM(List<string> itemData)
+        {
+            CartVMMapper cartVMMapper = new(itemData);
+
+            return cartVMMapper.ToCartVM();
+        }
+
+        public string ToString(CartVM cartVM)
+        {
+            return JsonConvert.SerializeObject(cartVM);
+        }
+
+        public string ToString(List<CartVM> cartVM)
+        {
+            return JsonConvert.SerializeObject(cartVM);
+        }
+    }
+}
