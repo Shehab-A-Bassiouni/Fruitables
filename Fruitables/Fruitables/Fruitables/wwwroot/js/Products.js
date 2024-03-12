b639124c344171ffd726ca1cb37ea76fc5e605ef
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-03-12 02:47:35 +0200

    Revert "Merge branch 'main' of https://github.com/Shehab-A-Bassiouni/Fruitables"
    This reverts commit 3faa481fdbacb2149867b2e0a4599a6014ab4238, reversing
    changes made to 16fe0c14840f087d2e6aa661403c4a4ba8ed495c.

---
+++ Fruitables/Fruitables/Fruitables/wwwroot/js/Products.js
@@ -0,0 +1,19 @@
+﻿
+const liveToastBtn = document.getElementById('liveToastBtn');
+const liveToast = new bootstrap.Toast(document.getElementById('liveToast'));
+
+liveToastBtn.addEventListener('click', function () {
+    try {
+
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+        cart.push(String(liveToastBtn.getAttribute('value')));
+        localStorage.setItem('cart', JSON.stringify(cart));
+        liveToast.show();
+    }
+    catch (e) {
+        console.error('Error', e);
+    }
+    });
+
+
+
---
+++ Fruitables/Fruitables/FruitablesBL/ViewModels/CartVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitablesBL.ViewModels
+{
+    public class CartVM
+    {
+        public string? ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal TotalPrice { get; set; }
+        public string? ItemImage { get; set; }
+
+
+    }
+}
